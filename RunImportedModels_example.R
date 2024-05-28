057d496a81d77ad72df80475ae03db092d9ca389
LeonieLorenz <leonie.lorenz96@web.de>
2024-05-28 12:05:26 +0100

    added more comments to the examples

--- RunImportedModels_example.R
+++ RunImportedModels_example.R
@@ -1,29 +1,38 @@
-library(odin)
+# you can find the models using the BioModels ID on https://www.ebi.ac.uk/biomodels/
 
+# we need odin to run the models
+# documentation: https://mrc-ide.github.io/odin/
+# install.packages("odin")
+library(odin)
+# install.packages("devtools")
+library(devtools)
+load_all() # loads functions from SBMLtoOdin package
+
+### example 1
 # SIR model example (Malaysia)
 importSBMLfromBioModels("BIOMD0000000982","../LawModel.R")
 
-model_generator <- odin::odin("../LawModel.R")
-LawModel <- model_generator$new()
-LawModel_res <- LawModel$run(0:150)
+model_generator <- odin::odin("../LawModel.R") # creates model
+LawModel <- model_generator$new() #creates object, with parameters if needed
+LawModel_res <- LawModel$run(0:150) # runs model forward in time, for specified time
 
 
-par(mar = c(4.1, 5.1, 0.5, 0.5), las = 1)
-cols <- c(S = "#000000", I = "#E69F00", R = "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+cols <- c(S = "#000000", I = "#E69F00", R = "#56B4E9") # defines colors for plot
 matplot(LawModel_res[, 1], LawModel_res[, c(3,4)], xlab = "Time", ylab = "Number of individuals",
         type = "l", col = cols[c(2,3)], lty = 1, ylim = c(0,7000))
 legend("topright", lwd = 1, col = cols, legend = c("S", "I", "R"), bty = "n")
+# S is not shown in this version of the plot because it is very large compared to I and R
 
 
-importSBMLfromBioModels("MODEL2307110001","../MothesModel.R")
-model_generator_mothes <- odin::odin("../MothesModel.R",verbose = FALSE)
-MothesModel <- model_generator_mothes$new()
-MothesModel_res <- MothesModel$run(0:400)
+### example 2
+# Model for Intracellular Signalling
+importSBMLfromBioModels("MODEL2307110001","../MothesModel.R") # imports model using SBMLtoOdin package
+model_generator_mothes <- odin::odin("../MothesModel.R",verbose = FALSE) # creates model
+MothesModel <- model_generator_mothes$new() #creates object, with parameters if needed
+MothesModel_res <- MothesModel$run(0:400) # runs model forward in time, for specified time
 
 
-
-par(mar = c(4.1, 5.1, 0.5, 0.5), las = 1)
-cols <- c(S = "#000000", I = "#E69F00", R = "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+cols <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 matplot(MothesModel_res[, 1], MothesModel_res[, -1], xlab = "Time", ylab = "Number of individuals",
         type = "l", col = cols, lty = 1, ylim = c(0,40))
 legend("topright", lwd = 1, col = cols, legend = colnames(MothesModel_res)[-1], bty = "n")
